a65e54dcc5a426ef48b182da739f91c235650475
ValentynaGorbachenko <gorbachenko.valentyna@gmail.com>
2017-03-18 22:45:04 -0700

    Update Person class with more patterns for phone validation

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -16,15 +16,24 @@ class Person{
     // primitive | protected | public
     public String phone;
     public boolean phoneValidate(String phone){
-        boolean valid = true;
-        String pattern = "\\(\\d{3}\\)\\d{3}\\-\\d{4}";
+        String pattern = "\\(\\d{3}\\)\\d{3}\\-\\d{4}"; // (123)456-7890
+        String pattern2 = "\\(\\d{3}\\)-\\d{3}\\-\\d{4}"; // (123)-456-7890
+        String pattern3 = "\\d{10}"; // 1234567890
+        String pattern4 = "\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}"; // 123-456-7890 or 123.456.7890 or 123 456 7890
         System.out.println("This is from sout "+phone.matches(pattern));
         if (!phone.matches(pattern)){
-            valid = false;
+            return true;
+        } else if (phone.matches(pattern2)){
+            return true;
+        } else if (phone.matches(pattern3)){
+            return true;
+        } else if (phone.matches(pattern4)){
+            return true;
+        } else {
+            return false;
         }
-        return valid;
     };
-    public Person (String phone2){
-        this.phone = phone2;
+    public Person (String personPhone){
+        this.phone = personPhone;
     }
 }
